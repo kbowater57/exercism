6a582c6acf173edc32a039f62ec7825de45e689a
Keir Bowater <kbowater57@gmail.com>
2022-02-18 10:10:28 +0000

    Added space-age exercise with beginnings of work

---
+++ cpp/space-age/space_age.cpp
@@ -0,0 +1,20 @@
+#include "space_age.h"
+
+namespace space_age {
+
+constexpr float mercury_year = 0.2408467;
+constexpr float venus_year = 0.61519726;
+constexpr float earth_year = 1.0;
+constexpr float mars_year = 1.8808158;
+constexpr float jupiter_year = 11.862615;
+constexpr float saturn_year = 29.447498;
+constexpr float uranus_year = 84.016846;
+constexpr float neptune_year = 164.79132;
+ 
+constexpr int earth_year_in_seconds = 31557600;
+
+class age {
+
+}
+
+}  // namespace space_age
