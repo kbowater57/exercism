c0ff749f0ee43f00c6ed3a9c68fe43b278ee61fc
Keir Bowater <kbowater57@gmail.com>
2022-02-18 14:53:04 +0000

    Passes first test in grains challenge

---
+++ cpp/grains/grains.cpp
@@ -0,0 +1,18 @@
+#include "grains.h"
+
+namespace grains {
+    unsigned long long square(int square)
+    {
+        return two_factorial(square - 1 );
+    }
+
+    unsigned long long two_factorial(int i)
+    {
+        unsigned long long result = 1;
+        while(i > 0){
+            result *= result;
+        }
+        
+        return result;
+    }
+}  // namespace grains
---
+++ cpp/grains/grains.h
@@ -0,0 +1,9 @@
+#if !defined(GRAINS_H)
+#define GRAINS_H
+
+namespace grains {
+    unsigned long long square(int square);
+    unsigned long long two_factorial(int i);
+}  // namespace grains
+
+#endif // GRAINS_H
