4f9a7bab14e11dadd41cbf705cbdad7d2463c8ca
Keir Bowater <kbowater57@gmail.com>
2022-02-18 11:36:07 +0000

    First test of space age challenge passed

--- cpp/space-age/space_age.cpp
+++ cpp/space-age/space_age.cpp
@@ -2,19 +2,30 @@
 
 namespace space_age {
 
-constexpr float mercury_year = 0.2408467;
-constexpr float venus_year = 0.61519726;
-constexpr float earth_year = 1.0;
-constexpr float mars_year = 1.8808158;
-constexpr float jupiter_year = 11.862615;
-constexpr float saturn_year = 29.447498;
-constexpr float uranus_year = 84.016846;
-constexpr float neptune_year = 164.79132;
+// constexpr float mercury_year = 0.2408467;
+// constexpr float venus_year = 0.61519726;
+// constexpr float earth_year = 1.0;
+// constexpr float mars_year = 1.8808158;
+// constexpr float jupiter_year = 11.862615;
+// constexpr float saturn_year = 29.447498;
+// constexpr float uranus_year = 84.016846;
+// constexpr float neptune_year = 164.79132;
  
-constexpr int earth_year_in_seconds = 31557600;
+// constexpr int earth_year_in_seconds = 31557600;
 
-class age {
 
-}
+
+// class space_age {
+//     private:
+//         int seconds_{};
+    
+//     public:
+//         space_age(int seconds)
+//         {
+//             seconds_ = seconds;
+//         }
+
+
+// };
 
 }  // namespace space_age
--- cpp/space-age/space_age.h
+++ cpp/space-age/space_age.h
@@ -2,7 +2,14 @@
 #define SPACE_AGE_H
 
 namespace space_age {
-
+    class space_age{
+        private:
+            int seconds_{};
+        
+        public:
+            space_age(int seconds){seconds_ = seconds;};
+            float seconds() const {return seconds_;};
+    };
 }  // namespace space_age
 
 #endif // SPACE_AGE_H
