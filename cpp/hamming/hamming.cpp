edb20be8c30b2fea7784d623289b73fcab61c556
Keir Bowater <kbowater57@gmail.com>
2022-02-21 12:29:15 +0000

    Passes first test in hamming challenge

---
+++ cpp/hamming/hamming.cpp
@@ -0,0 +1,12 @@
+#include "hamming.h"
+
+namespace hamming {
+    int compute(std::string s1, std::string s2)
+    {
+        if(s1 == s2)
+        {
+            return 0;
+        }
+        return {};
+    }
+}  // namespace hamming
---
+++ cpp/hamming/hamming.h
@@ -0,0 +1,10 @@
+#if !defined(HAMMING_H)
+#define HAMMING_H
+
+#include <string>
+
+namespace hamming {
+    int compute(std::string s1, std::string s2);
+}  // namespace hamming
+
+#endif // HAMMING_H
