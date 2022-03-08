5df4d51dc61cab9e5d5f5f1a38f38462f22f2818
Keir Bowater <kbowater57@gmail.com>
2022-03-08 14:18:05 +0000

    Passes first four tests in queen-attack challenge

--- cpp/queen-attack/queen_attack.cpp
+++ cpp/queen-attack/queen_attack.cpp
@@ -1,4 +1,32 @@
 #include "queen_attack.h"
 
 namespace queen_attack {
+
+    chess_board::chess_board(const std::pair<int, int>& white,
+                             const std::pair<int, int>& black) :
+        white_(white),
+        black_(black)
+        {
+            if(white == black)
+            {
+                throw std::domain_error("The queens have to be in different places");
+            }
+        }
+
+    chess_board::operator std::string() const
+    {
+        std::string board_string{
+        "_ _ _ _ _ _ _ _\n"
+        "_ _ _ _ _ _ _ _\n"
+        "_ _ _ _ _ _ _ _\n"
+        "_ _ _ _ _ _ _ _\n"
+        "_ _ _ _ _ _ _ _\n"
+        "_ _ _ _ _ _ _ _\n"
+        "_ _ _ _ _ _ _ _\n"
+        "_ _ _ _ _ _ _ _\n"};
+        board_string[16*white_.first + 2*white_.second] = 'W';
+        board_string[16*black_.first + 2*black_.second] = 'B';
+        return board_string;
+    }
+
 }  // namespace queen_attack
--- cpp/queen-attack/queen_attack.h
+++ cpp/queen-attack/queen_attack.h
@@ -3,6 +3,7 @@
 
 #include <utility>
 #include <stdexcept>
+#include <string>
 
 namespace queen_attack {
     class chess_board
@@ -14,19 +15,13 @@ namespace queen_attack {
         public:
             auto white() const {return white_;}
             auto black() const {return black_;}
+            operator std::string() const;
 
             chess_board(const std::pair<int, int>& white = std::make_pair(0,3),
-                        const std::pair<int, int>& black = std::make_pair(7,3))
-                         :
+                        const std::pair<int, int>& black = std::make_pair(7,3));
+                         
 
-                white_(white),
-                black_(black)
-                {
-                    if(white == black)
-                    {
-                        throw std::domain_error("The queens have to be in different places");
-                    }
-                }
+
     };
 }  // namespace queen_attack
 
