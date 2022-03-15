bd4e10b7c888fc78a4cdf4dbca81f815949c2077
Keir Bowater <kbowater57@gmail.com>
2022-03-15 09:54:55 +0000

    Iterate again on nth-prime challenge following feedback

--- cpp/nth-prime/nth_prime.cpp
+++ cpp/nth-prime/nth_prime.cpp
@@ -4,7 +4,9 @@ namespace nth_prime {
     int nth(int input)
     {
         if ( input == 0 ){throw std::domain_error("The 0th prime is undefined");}
-        std::pair<int, int> which_prime{2,1}; //first prime is two
+        std::pair<int, int> which_prime{2,1};
+        // first value is value of most recently found prime
+        // second value is which prime this is, i.e. 2 is the first prime
         int candidate_prime = 3;
 
         while(which_prime.second != input)
@@ -21,12 +23,12 @@ namespace nth_prime {
     }
 
 
-    bool is_prime(int input)
+    bool is_prime(int candidate)
     {
 
-        for (int i = int(floor(sqrt(input))) ; i > 1 ; --i)
+        for (int i = int(floor(sqrt(candidate))) ; i > 1 ; --i)
         {
-            if (input % i == 0)
+            if (candidate % i == 0)
             {
                 return false;
             }
