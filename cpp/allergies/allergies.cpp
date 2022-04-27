d0813f7b88c205ff05b37c7bd36d651e3e444c35
Keir Bowater <kbowater57@gmail.com>
2022-04-27 15:28:48 +0100

    Passes all tests in allergies challenge. Challenge complete!

--- cpp/allergies/allergies.cpp
+++ cpp/allergies/allergies.cpp
@@ -3,15 +3,21 @@
 namespace allergies {
     std::unordered_set<std::string> allergy_test::get_allergies()
     {
-        // bool allergic = score_ & (1 << position);
-        return {};
+        std::unordered_set<std::string> allergic_set;
+        for(unsigned int position = 0; position < allergen_vector_.size(); ++position)
+        {
+            bool allergic = score_ & (1 << position);
+            if(allergic)
+            {
+                allergic_set.emplace(allergen_vector_[position]);
+            }
+        }
+        return allergic_set;
     }
     
     bool allergy_test::is_allergic_to(std::string allergen)
     {
-
-        // int position = allergen_map_.at(allergen);
-        int position = std::find(allergen_vector_.begin(), allergen_vector_.end(),allergen) - allergen_vector_.begin();
+        int position = std::find(allergen_vector_.begin(), allergen_vector_.end(), allergen) - allergen_vector_.begin();
 
         bool allergic = score_ & (1 << position);
         return allergic;
