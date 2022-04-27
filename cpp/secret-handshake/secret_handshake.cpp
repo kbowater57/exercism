5463c6dfae1305e0de39e42925f52b42e0eca11c
Keir Bowater <kbowater57@gmail.com>
2022-04-27 11:09:26 +0100

    Sets up rest of secret handshake challenge

--- cpp/secret-handshake/secret_handshake.cpp
+++ cpp/secret-handshake/secret_handshake.cpp
@@ -3,7 +3,15 @@
 namespace secret_handshake {
     std::vector<std::string> commands(int signal)
     {
-        ++signal;
-        return {"wink"};
+        std::vector<std::string> output;
+        for(int position = 0 ; position < 5 ; ++position)
+        {
+            bool bit = signal & (1 << position);
+            if(position == 0 and bit == 1)
+            {
+                output.push_back("wink");
+            }
+        }
+        return output;
     }
 }  // namespace secret_handshake
