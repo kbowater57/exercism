38abebe2551e1c640f0259ea611d6bfd97703c0e
Keir Bowater <kbowater57@gmail.com>
2022-04-27 14:20:16 +0100

    Change impl so useful for future tests in allergies challenge

--- cpp/allergies/allergies.cpp
+++ cpp/allergies/allergies.cpp
@@ -1,49 +1,19 @@
 #include "allergies.h"
 
 namespace allergies {
+    std::unordered_set<std::string> allergy_test::get_allergies()
+    {
+        // bool allergic = score_ & (1 << position);
+        return {};
+    }
+    
     bool allergy_test::is_allergic_to(std::string allergen)
     {
-        const std::map<std::string, int> allergen_map = 
-        {
-            {"eggs", 0},
-            {"peanuts", 1},
-            {"shellfish", 2},
-            {"strawberries", 3},
-            {"tomatoes", 4},
-            {"chocolate", 5},
-            {"pollen", 6},
-            {"cats", 7}
-        };
 
-        int position = allergen_map.at(allergen);
+        // int position = allergen_map_.at(allergen);
+        int position = std::find(allergen_vector_.begin(), allergen_vector_.end(),allergen) - allergen_vector_.begin();
 
         bool allergic = score_ & (1 << position);
         return allergic;
-        // const std::vector<std::string> allergen_list{"eggs", "peanuts"}
-        // for(int position = 0 ; position < 8 ; ++position)
-        // {
-
-            // if(position == 1 and bit == 1)
-            // {
-            //     output.push_back("double blink");
-            // }
-
-            // if(position == 2 and bit == 1)
-            // {
-            //     output.push_back("close your eyes");
-            // }
-
-            // if(position == 3 and bit == 1)
-            // {
-            //     output.push_back("jump");
-            // }
-
-            // if(position == 4 and bit == 1)
-            // {
-            //     std::reverse(output.begin(), output.end());
-            // }
-
-        // }
-        return false;
     }
 }  // namespace allergies
--- cpp/allergies/allergies.h
+++ cpp/allergies/allergies.h
@@ -4,19 +4,38 @@
 #include <string>
 #include <vector>
 #include <map>
+#include <unordered_set>
 
 namespace allergies {
     class allergy_test
     {
         public:
-            int score_;
+            bool is_allergic_to(std::string allergen);
+            std::unordered_set<std::string> get_allergies();
 
             allergy_test(int score)
             {
                 score_ = score;
             }
+        
+        private:
+            const std::map<std::string, int> allergen_map_ = 
+            {
+                {"eggs", 0},
+                {"peanuts", 1},
+                {"shellfish", 2},
+                {"strawberries", 3},
+                {"tomatoes", 4},
+                {"chocolate", 5},
+                {"pollen", 6},
+                {"cats", 7}
+            };
 
-            bool is_allergic_to(std::string allergen);
+            const std::vector<std::string> allergen_vector_ =
+            { "eggs", "peanuts", "shellfish", "strawberries", "tomatoes",
+              "chocolate", "pollen", "cats"};
+
+            int score_;
     };
 }  // namespace allergies
 
