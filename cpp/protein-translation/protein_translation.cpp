d0c67fb0160d4c5d09f4c0a0acd4c907fb01ed25
Keir Bowater <kbowater57@gmail.com>
2022-04-28 15:09:49 +0100

    Passes first two tests in protein translation challenge

--- cpp/protein-translation/protein_translation.cpp
+++ cpp/protein-translation/protein_translation.cpp
@@ -3,9 +3,18 @@
 namespace protein_translation {
 
 std::map<std::string, std::string> codon_protein_map =
-{   {"AUG","Methionine"}};
+{   {"AUG","Methionine"},
+    {"UUU","Phenylalanine"}
+};
 
-
+// AUG	Methionine
+// UUU, UUC	Phenylalanine
+// UUA, UUG	Leucine
+// UCU, UCC, UCA, UCG	Serine
+// UAU, UAC	Tyrosine
+// UGU, UGC	Cysteine
+// UGG	Tryptophan
+// UAA, UAG, UGA	STOP
 std::vector<std::string> proteins(std::string rna_sequence)
 {
     std::vector<std::string> output = {};
