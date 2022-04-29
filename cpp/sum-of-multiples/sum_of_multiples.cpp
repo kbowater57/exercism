99bb039ce7b88aef18b70f457472f470f1a7304b
Keir Bowater <kbowater57@gmail.com>
2022-04-29 16:03:55 +0100

    Passes first test in sum of multiples challenge

---
+++ cpp/sum-of-multiples/sum_of_multiples.cpp
@@ -0,0 +1,10 @@
+#include "sum_of_multiples.h"
+
+namespace sum_of_multiples {
+    int to(std::list<int> multiples, int limit)
+    {
+        ++multiples.begin();
+        ++limit;
+        return 0;
+    }
+}  // namespace sum_of_multiples
---
+++ cpp/sum-of-multiples/sum_of_multiples.h
@@ -0,0 +1,10 @@
+#if !defined(SUM_OF_MULTIPLES_H)
+#define SUM_OF_MULTIPLES_H
+
+#include <list>
+
+namespace sum_of_multiples {
+    int to(std::list<int> multiples, int limit);
+}  // namespace sum_of_multiples
+
+#endif // SUM_OF_MULTIPLES_H
