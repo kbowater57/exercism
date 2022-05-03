1b93ce3028d2be18837e2a8803761c82deaeff06
Keir Bowater <kbowater57@gmail.com>
2022-05-03 15:26:12 +0100

    Passes all tests in prime factors challenge, challenge complete

--- cpp/prime-factors/prime_factors.cpp
+++ cpp/prime-factors/prime_factors.cpp
@@ -1,8 +1,23 @@
 #include "prime_factors.h"
 
 namespace prime_factors {
+    std::vector<int> primes = {2,3,5,7,11,13,17,19,23,461};
+
     std::vector<int> of(int input){
-        ++input;
-        return {};
+        std::vector<int> result;
+        int i = 0;
+        while (input != 1)
+        {
+            if (input % primes[i] == 0)
+            {
+                result.push_back(primes[i]);
+                input /= primes[i];
+            }
+            else
+            {
+                ++i;
+            }
+        }
+        return result;
     }
 }  // namespace prime_factors
