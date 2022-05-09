3521d0114dc7f8c3a335dafabed560773c70e743
Keir Bowater <kbowater57@gmail.com>
2022-05-09 12:44:12 +0100

    Reimplements in a simpler way, passes all time cases in clock challenge

--- cpp/clock/clock.cpp
+++ cpp/clock/clock.cpp
@@ -1,7 +1,7 @@
 #include "clock.h"
 
 namespace date_independent {
-    std::string clock::at(int hour, int minute)
+    clock clock::at(int hour, int minute)
     {
         int total_minutes = 60 * hour + minute;
         int total_hours = total_minutes / 60;
@@ -17,23 +17,12 @@ namespace date_independent {
         {
             total_hours_mod_24 = 24 + (total_hours_mod_24 % 24);
         }
-        // if( minute < 0)
-        // {
-        //     minute = 60 + (minute % 60);
-        // }
-        // int modified_hour = (hour + minute / 60)% 24;
-        std::string hour_component = std::to_string(total_hours_mod_24);
-        if( total_hours_mod_24 < 10)
-        {
-            hour_component = "0" + hour_component;
-        }
         
-        // int modified_minute = minute % 60;
-        std::string minute_component = std::to_string(total_minutes_mod_60);
-        if( total_minutes_mod_60 < 10)
-        {
-            minute_component = "0" + minute_component;
-        }
-        return hour_component + ":" + minute_component;
+        return clock (total_hours_mod_24, total_minutes_mod_60);
     }
+
+    // std::string plus(int mins_to_add)
+    // {
+    //     int hours ;
+    // }
 }  // namespace date_independent
--- cpp/clock/clock.h
+++ cpp/clock/clock.h
@@ -4,12 +4,39 @@
 #include <string>
 
 namespace date_independent {
-    namespace clock{
-    // class clock
-    // {
-    //     public:
-    //         std::string at(int hour, int minute);
-    // };
+
+    class clock
+    {
+        public:
+            static clock at(int hour, int minute);
+
+            clock(int hour, int minute)
+            {
+                hour_ = hour;
+                minute_ = minute;
+            };
+
+            explicit operator std::string() const 
+            {
+                std::string hour_component = std::to_string(hour_);
+                if( hour_ < 10)
+                {
+                    hour_component = "0" + hour_component;
+                }
+
+                std::string minute_component = std::to_string(minute_);
+                if( minute_ < 10)
+                {
+                    minute_component = "0" + minute_component;
+                }
+
+                return hour_component + ":" + minute_component;
+            }
+            
+        private:
+            int hour_;
+            int minute_;
+    };
     // struct clock
     // {
     //     int hour;
@@ -17,8 +44,9 @@ namespace date_independent {
 
     //     std::string at(int hour, int minute);        
     // };
-    std::string at(int hour, int minute); 
-    }
+    // std::string at(int hour, int minute);
+    // std::string plus(int mins_to_add);
+    // }
 
 }  // namespace date_independent
 
