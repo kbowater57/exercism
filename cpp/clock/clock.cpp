c660772b9a38a0c8cfc0df63c3a41997c3093b16
Keir Bowater <kbowater57@gmail.com>
2022-05-09 10:26:44 +0100

    Passes all time cases in clock challenge

--- cpp/clock/clock.cpp
+++ cpp/clock/clock.cpp
@@ -3,20 +3,34 @@
 namespace date_independent {
     std::string clock::at(int hour, int minute)
     {
-        if( hour < 0)
+        int total_minutes = 60 * hour + minute;
+        int total_hours = total_minutes / 60;
+        int total_hours_mod_24 = total_hours % 24;
+        int total_minutes_mod_60 = total_minutes % 60;
+        if( total_minutes_mod_60 < 0)
         {
-            hour = 24 + (hour % 24);
+            total_hours_mod_24 -= 1;
+            total_minutes_mod_60 = 60 + (total_minutes_mod_60 % 60);
         }
-        int modified_hour = (hour + minute / 60)% 24;
-        std::string hour_component = std::to_string(modified_hour);
-        if( modified_hour < 10)
+
+        if( total_hours_mod_24 < 0)
+        {
+            total_hours_mod_24 = 24 + (total_hours_mod_24 % 24);
+        }
+        // if( minute < 0)
+        // {
+        //     minute = 60 + (minute % 60);
+        // }
+        // int modified_hour = (hour + minute / 60)% 24;
+        std::string hour_component = std::to_string(total_hours_mod_24);
+        if( total_hours_mod_24 < 10)
         {
             hour_component = "0" + hour_component;
         }
         
-        int modified_minute = minute % 60;
-        std::string minute_component = std::to_string(modified_minute);
-        if( modified_minute < 10)
+        // int modified_minute = minute % 60;
+        std::string minute_component = std::to_string(total_minutes_mod_60);
+        if( total_minutes_mod_60 < 10)
         {
             minute_component = "0" + minute_component;
         }
